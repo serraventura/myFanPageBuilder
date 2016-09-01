8baf974b798e538b180fbb087f5ac9f3fe8084f8
serraventura <serraventura@gmail.com>
2016-09-01 22:12:29 +0100

    applying thunk middleware

--- client/index.js
+++ client/index.js
@@ -3,12 +3,11 @@ import * as React from "react";
 import {render} from "react-dom";
 import {Router, Route, hashHistory} from "react-router";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
 
-import rootReducer from "./reducers/root";
 import App from "./components/app";
+import configureStore from "./store/configureStore";
 
-const store = createStore(rootReducer);
+const store = configureStore();
 
 render(
 	<Provider store={store}>
---
+++ client/store/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from "./../reducers/root";
+
+export default function configureStore(preloadedState) {
+    const store = createStore(
+        rootReducer,
+        preloadedState,
+        applyMiddleware(thunkMiddleware)
+    )
+
+    return store;
+
+}
