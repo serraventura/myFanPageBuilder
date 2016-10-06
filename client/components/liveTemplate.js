3a6c02665623f483f210f30c398cfd4602eb9904
serraventura <serraventura@gmail.com>
2016-10-06 16:53:56 +0100

    painel menu options static

--- client/components/liveTemplate.js
+++ client/components/liveTemplate.js
@@ -6,7 +6,53 @@ const LiveTemplate = (props) => (
         className="live-template-component"
         style={props.open ? UNHIDE : HIDE}
     >
-        <div className="live-template-painel"></div>
+        <div className="live-template-painel">
+
+            <h3>Template configuration</h3>
+
+            <div><input type="checkbox" id="useFanpageCover" /><label htmlFor="useFanpageCover">Use Fan Page cover picture</label></div>
+
+            <div className="live-template-painel-subpainel-left">
+                <h4>Menu</h4>
+                <ul className="live-template-menu-items">
+                    <li className="active">About Us</li>
+                    <li>Photos</li>
+                    <li>Blog</li>
+                    <li>Services</li>
+                </ul>
+            </div>
+
+            <div className="live-template-painel-subpainel-right">
+                <h4>Menu Options</h4>
+                <ul>
+                    <li>
+                        Name: <input type="text" />
+                    </li>
+                    <li>
+                        Initial Page: <input type="checkbox" />
+                    </li>
+                    <li>
+                        Location: <input type="checkbox" />
+                    </li>
+                    <li>
+                        Active: <input type="checkbox" />
+                    </li>
+                    <li>
+                        Hashtag: <input type="text" />
+                    </li>
+                    <li>
+                        Active: <input type="checkbox" />
+                    </li>
+                    <li>
+                        Active: <input type="checkbox" />
+                    </li>                                                            
+                </ul>
+
+            </div>
+
+            <button>Close</button>
+
+        </div>
         <div className="live-template-footer"></div>
         <iframe 
             key={props.selectedTemplate}
